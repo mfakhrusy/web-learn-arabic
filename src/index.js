0298802697f242c9b1e286399b6bca648fae8061
Muhamad Fakhrusy <v8areu@gmail.com>
2018-03-25 11:59:27 +0700

    edit some config files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'styles/css/index.css';
 import App from 'components/App';
+import 'styles/css/main.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
